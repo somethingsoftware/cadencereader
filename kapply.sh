4143937cb190773a324477bbf60d9ce0a121e813
samiam2013 <samiam2013@gmail.com>
2026-09-12 23:18:47 -0500

    remove local registry

--- kapply.sh
+++ kapply.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Build the image using the local registry tag
-docker build -f apps/web/main/Dockerfile -t localhost:30500/cadencereader:latest .
-docker push localhost:30500/cadencereader:latest
-
-docker build -f apps/web/dripper/Dockerfile -t localhost:30500/dripper:latest .
-docker push localhost:30500/dripper:latest
-
-docker build -f apps/rssimport/Dockerfile -t localhost:30500/rssimport:latest .
-docker push localhost:30500/rssimport:latest
-
 kubectl apply -f k8s/
 kubectl rollout restart deployment cadencereader 
 kubectl delete job immediate-rssimport # in case it already exists
