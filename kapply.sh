de47e981dad93eb1fdbc69af321aee10c3f088f3
samiam2013 <samiam2013@gmail.com>
2026-09-13 00:56:33 -0500

    update localhost -> 0.0.0.0

--- kapply.sh
+++ kapply.sh
@@ -3,5 +3,4 @@ set -euo pipefail
 
 kubectl apply -f k8s/
 kubectl rollout restart deployment cadencereader 
-kubectl delete job immediate-rssimport # in case it already exists
-kubectl create job immediate-rssimport --from=cronjob/rssimport
+kubectl rollout restart deployment dripper
